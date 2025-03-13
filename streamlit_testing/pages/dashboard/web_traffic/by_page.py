3b2b8ed61e57cbd8e4995c85c98ef79b63ad7514
Philip Nye <philipnye@users.noreply.github.com>
2025-03-13 10:05:03 +0000

    Single quotes -> double quotes

--- streamlit_testing/pages/dashboard/web_traffic/by_page.py
+++ streamlit_testing/pages/dashboard/web_traffic/by_page.py
@@ -9,18 +9,18 @@ import ds_utils.database_operations as dbo
 
 # CONNECT TO DATABASE
 connection = dbo.connect_sql_db(
-    driver='pyodbc',
-    driver_version=os.environ['ODBC_DRIVER'],
-    dialect='mssql',
-    server=os.environ['ODBC_SERVER'],
-    database=os.environ['ODBC_DATABASE'],
-    authentication=os.environ['ODBC_AUTHENTICATION'],
-    username=os.environ['AZURE_CLIENT_ID'],
-    password=os.environ['AZURE_CLIENT_SECRET'],
+    driver="pyodbc",
+    driver_version=os.environ["ODBC_DRIVER"],
+    dialect="mssql",
+    server=os.environ["ODBC_SERVER"],
+    database=os.environ["ODBC_DATABASE"],
+    authentication=os.environ["ODBC_AUTHENTICATION"],
+    username=os.environ["AZURE_CLIENT_ID"],
+    password=os.environ["AZURE_CLIENT_SECRET"],
 )
 
 # RUN QUERY
-with open('streamlit_testing/sql/dashboard/by_page.sql', 'r') as file:
+with open("streamlit_testing/sql/dashboard/by_page.sql", "r") as file:
     script = file.read()
 
 try:
