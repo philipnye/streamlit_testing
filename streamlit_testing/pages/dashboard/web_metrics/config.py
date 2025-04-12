2d4f721b100caebdf2a6bae65d619bd6b08f74c7
Philip Nye <philipnye@users.noreply.github.com>
2025-04-12 22:43:01 +0100

    Alphabetise vars

--- streamlit_testing/pages/dashboard/web_metrics/config.py
+++ streamlit_testing/pages/dashboard/web_metrics/config.py
@@ -2,15 +2,40 @@ from streamlit_testing.pages.dashboard.web_metrics.utils import (
     format_integer, format_decimal, format_percentage, format_time
 )
 
-metrics_by_page = [
-    "Page title",
-    "URL",
+breakdowns = [
     "Content type",
-    "Published date",
-    "Updated date",
-    "Authors",
-    "Research areas",
-    "Tags",
+    "Publication type",
+    "Research area",
+    "Tag",
+    "Author",
+    "Published date: year",
+    "Published date: month",
+    "Published date: day",
+    "Updated date: year",
+    "Updated date: month",
+    "Updated date: day",
+]
+date_ranges = {
+    "Last 7 days": 7,
+    "Last 30 days": 30,
+    "Last 90 days": 90,
+    "Last 12 months": 365,
+    "Year to date": None,
+    "Custom": None,
+}
+default_breakdowns = "Content type"
+default_date_range = "Last 30 days"
+default_download_metric = "Downloads"
+default_web_traffic_metric = "Page views"
+download_metric_aggregations = {
+    "Downloads": ("Downloads", "sum"),
+}
+download_metric_calculations = None
+download_metrics_display = {
+    "Downloads": format_integer,
+}
+download_metrics_raw = [
+    "Downloads",
 ]
 metrics_by_output = [
     "Output title",
@@ -23,13 +48,15 @@ metrics_by_output = [
     "Research areas",
     "Tags",
 ]
-web_traffic_metrics_raw = [
-    "Page views",
-    "Active users",
-    "Sessions",
-    "Engaged sessions",
-    "User engagement duration",
-    "Downloads",
+metrics_by_page = [
+    "Page title",
+    "URL",
+    "Content type",
+    "Published date",
+    "Updated date",
+    "Authors",
+    "Research areas",
+    "Tags",
 ]
 web_traffic_metric_aggregations = {
     "Page views": ("Page views", "sum"),
@@ -70,38 +97,11 @@ web_traffic_metrics_display = {
     "Downloads": format_integer,
     "Download rate": format_percentage,
 }
-default_web_traffic_metric = "Page views"
-download_metrics_raw = [
+web_traffic_metrics_raw = [
+    "Page views",
+    "Active users",
+    "Sessions",
+    "Engaged sessions",
+    "User engagement duration",
     "Downloads",
 ]
-download_metric_aggregations = {
-    "Downloads": ("Downloads", "sum"),
-}
-download_metric_calculations = None
-download_metrics_display = {
-    "Downloads": format_integer,
-}
-default_download_metric = "Downloads"
-default_breakdowns = "Content type"
-breakdowns = [
-    "Content type",
-    "Publication type",
-    "Research area",
-    "Tag",
-    "Author",
-    "Published date: year",
-    "Published date: month",
-    "Published date: day",
-    "Updated date: year",
-    "Updated date: month",
-    "Updated date: day",
-]
-date_ranges = {
-    "Last 7 days": 7,
-    "Last 30 days": 30,
-    "Last 90 days": 90,
-    "Last 12 months": 365,
-    "Year to date": None,
-    "Custom": None,
-}
-default_date_range = "Last 30 days"
