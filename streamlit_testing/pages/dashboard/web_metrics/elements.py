2d3a345d380da4b06461cf382050b15458a663e5
Philip Nye <philipnye@users.noreply.github.com>
2025-06-30 17:19:22 +0100

    Remove leading zero from axis label dates

--- streamlit_testing/pages/dashboard/web_metrics/elements.py
+++ streamlit_testing/pages/dashboard/web_metrics/elements.py
@@ -325,7 +325,7 @@ def draw_line_chart_section(
                 ),
                 gridcolor=COLOURS["grey_lighter_80pct"],
                 fixedrange=True,
-                tickformat="%a %d %b",
+                tickformat="%a %e %b",
             ),
             yaxis=dict(
                 zeroline=True,
