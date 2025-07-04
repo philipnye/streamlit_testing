91cde1249709c7c9297c92dc9ea1a25de3224552
Philip Nye <philipnye@users.noreply.github.com>
2025-07-04 17:21:31 +0100

    Bring downloads logic in line with by_output page

--- streamlit_testing/sql/dashboard/web_metrics/home_downloads.sql
+++ streamlit_testing/sql/dashboard/web_metrics/home_downloads.sql
@@ -1,17 +1,21 @@
+-- NB: By using an outer apply to corporate.content_basic_metadata_canonical this picks up
+-- details of the page from which an output is most commonly downloaded
 select top 10
-    pt.page_title [Page title],
-    pv.url URL,
-    sum(d.event_count) [Downloads]
-from corporate.page_views_canonical pv
-    left join corporate.content_basic_metadata_canonical bm on
-        pv.url = bm.url
+    pt.page_title [Output title],
+    d.file_name [File name],
+    sum(d.event_count) Downloads
+from corporate.downloads_canonical d
+    outer apply (
+        select top 1 *
+        from corporate.content_basic_metadata_canonical bm
+        where
+            d.url = bm.url
+    ) bm
     left join corporate.content_page_titles_canonical pt on
         bm.url = pt.url
-    left join corporate.downloads_canonical d on
-        pv.url = d.url and
-        pv.date = d.date
 where
-    pv.date between ? and ? and
+    pt.page_title is not null and
+    d.date between ? and ? and
     bm.content_type = ? and
     (
         (
@@ -25,6 +29,6 @@ where
     )
 group by
     pt.page_title,
-    pv.url
+    d.file_name
 order by
     sum(d.event_count) desc;
