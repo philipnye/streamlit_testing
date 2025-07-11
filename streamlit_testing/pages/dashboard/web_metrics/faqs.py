510d0bc0c7686df131dfc37b1077cbc851ce4edb
Philip Nye <philipnye@users.noreply.github.com>
2025-07-11 15:38:39 +0100

    Fix last updated date

--- streamlit_testing/pages/dashboard/web_metrics/faqs.py
+++ streamlit_testing/pages/dashboard/web_metrics/faqs.py
@@ -4,7 +4,7 @@ import streamlit_testing.pages.dashboard.web_metrics.elements as elements
 
 # DRAW PAGE HEADER
 st.title("Frequently asked questions")
-elements.draw_last_updated_badge("2025-07-10")
+elements.draw_last_updated_badge("2025-07-11")
 st.markdown("\n\n")
 st.markdown("\n\n")
 
