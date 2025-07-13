5ca5e778fd4b06a0528efb280d547cb26a3e051d
Philip Nye <philipnye@users.noreply.github.com>
2025-07-13 13:07:12 +0100

    Reflect use of most common download URL in corporate.downloads_canonical

--- streamlit_testing/sql/dashboard/web_metrics/by_output.sql
+++ streamlit_testing/sql/dashboard/web_metrics/by_output.sql
@@ -1,5 +1,3 @@
--- NB: By using an outer apply to corporate.content_basic_metadata_canonical this picks up
--- details of the page from which an output is most commonly downloaded
 select
     d.date Date,
     pt.page_title [Output title],
@@ -15,20 +13,16 @@ select
     p.topic Topic,
     d.event_count Downloads
 from corporate.downloads_canonical d
-    outer apply (
-        select top 1 *
-        from corporate.content_basic_metadata_canonical bm
-        where
-            d.url = bm.url
-    ) bm
+    left join corporate.content_basic_metadata_canonical bm on
+        d.url = d.url
     left join corporate.content_page_titles_canonical pt on
-        bm.url = pt.url
+        d.url = pt.url
     outer apply (
         select
             string_agg(t.team, ', ') team
         from corporate.content_teams_canonical t
         where
-            bm.url = t.url
+            d.url = t.url
         group by
             t.url
     ) t
@@ -37,7 +31,7 @@ from corporate.downloads_canonical d
             string_agg(p.topic, ', ') topic
         from corporate.content_topics_canonical p
         where
-            bm.url = p.url
+            d.url = p.url
         group by
             p.url
     ) p
@@ -46,7 +40,7 @@ from corporate.downloads_canonical d
             string_agg(a.author, ', ') author
         from corporate.content_authors_canonical a
         where
-            bm.url = a.url
+            d.url = a.url
         group by
             a.url
     ) a
--- streamlit_testing/sql/dashboard/web_metrics/home_downloads.sql
+++ streamlit_testing/sql/dashboard/web_metrics/home_downloads.sql
@@ -1,19 +1,13 @@
--- NB: By using an outer apply to corporate.content_basic_metadata_canonical this picks up
--- details of the page from which an output is most commonly downloaded
 select top 10
     pt.page_title [Output title],
     d.file_name_clean [File name],
     d.file_path_dedupe [Link],
     sum(d.event_count) Downloads
 from corporate.downloads_canonical d
-    outer apply (
-        select top 1 *
-        from corporate.content_basic_metadata_canonical bm
-        where
-            d.url = bm.url
-    ) bm
+    left join corporate.content_basic_metadata_canonical bm on
+        d.url = bm.url
     left join corporate.content_page_titles_canonical pt on
-        bm.url = pt.url
+        d.url = pt.url
 where
     pt.page_title is not null and
     d.date between ? and ? and
