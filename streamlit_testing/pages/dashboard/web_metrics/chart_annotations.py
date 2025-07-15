07b4d7d0dd58276510be8ac1483f5ac2a469da42
Philip Nye <philipnye@users.noreply.github.com>
2025-07-15 16:50:06 +0100

    Simplify chart annotations

--- streamlit_testing/pages/dashboard/web_metrics/chart_annotations.py
+++ streamlit_testing/pages/dashboard/web_metrics/chart_annotations.py
@@ -2,14 +2,14 @@ CHART_ANNOTATIONS = [
     {
         "start_date": "2023-11-07",
         "end_date": "2023-12-04",
-        "text": "A problem recording web traffic statistics means we do not have accurate data for this period",
+        "text": "Accurate data not available",
         "type": "range",
         "position": "end"
     },
     {
         "start_date": "2024-11-13",
         "end_date": "2025-01-13",
-        "text": "A change in how Google processed web traffic means we do not have accurate data for this period",
+        "text": "Accurate data not available",
         "type": "range",
         "position": "end"
     },
