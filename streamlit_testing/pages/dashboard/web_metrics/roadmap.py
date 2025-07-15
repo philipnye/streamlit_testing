746a5e118935779242604df06bac7fca09fcf51f
Philip Nye <philipnye@users.noreply.github.com>
2025-07-15 17:30:45 +0100

    Update last updated date

--- streamlit_testing/pages/dashboard/web_metrics/roadmap.py
+++ streamlit_testing/pages/dashboard/web_metrics/roadmap.py
@@ -4,7 +4,7 @@ import streamlit_testing.pages.dashboard.web_metrics.elements as elements
 
 # DRAW PAGE HEADER
 st.title("Roadmap")
-elements.draw_last_updated_badge("2025-07-14")
+elements.draw_last_updated_badge("2025-07-15")
 st.markdown("\n\n")
 st.markdown("\n\n")
 
